3105d1d64445fcefeeaf027493b684cbabea8d82
newstar <maori36@naver.com>
2023-08-09 23:05:37 +0900

    23.08.09

--- board/src/main/java/com/example/board/mapper/BoardMapper.java
+++ board/src/main/java/com/example/board/mapper/BoardMapper.java
@@ -10,4 +10,7 @@ import com.example.board.domain.vo.BoardVO;
 public interface BoardMapper {
 	public List<BoardVO> getList();
 	public void insert(BoardVO board);
+	public BoardVO read(Long bno);
+	public int delete(Long bno);
+	public int update(BoardVO board);
 }
--- board/src/test/java/com/example/board/mapper/BoardMapperTests.java
+++ board/src/test/java/com/example/board/mapper/BoardMapperTests.java
@@ -17,20 +17,54 @@ public class BoardMapperTests {
 	@Autowired
 	private BoardMapper boardMappper;
 	
-	@Test
-	public void getListTest() {
-		boardMappper.getList().forEach(log::info);
-	}
+//	@Test
+//	public void getListTest() {
+//		boardMappper.getList().forEach(log::info);
+//	}
+	
+//	@Test
+//	public void insertTest() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성한 글 제목05");
+//		board.setContent("새로 작성한 내용05");
+//		board.setWriter("user05");
+//		
+//		boardMappper.insert(board);
+//		
+//		log.info(board);
+//	}
+	
+//	@Test
+//	public void readTest() {
+//		BoardVO baordVO = boardMappper.read(5L);
+//		log.info(baordVO);
+//	}
 	
 	@Test
-	public void insertTest() {
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성한 글 제목03");
-		board.setContent("새로 작성한 내용03");
-		board.setWriter("user03");
-		
-		boardMappper.insert(board);
-		
-		log.info(board);
+	public void deleteTest() {
+		Long bno = 2L;
+		BoardVO boardVO = boardMappper.read(bno);
+		if(boardMappper.read(bno) != null) {
+			log.info("DELETE COUNT :" + boardMappper.delete(bno));	
+			return;
+		}
+		log.info("NO BOARD!");
 	}
+	
+//	@Test
+//	public void updateTest() {
+//		BoardVO boardVO = boardMappper.read(1L);
+//		if(boardVO != null) {
+//			
+//			boardVO.setTitle("수정된 제목");
+//			boardVO.setContent("수정된 내용");
+//			boardVO.setWriter("user00");
+//			
+//			log.info("UPDATE COUNT :" + boardMappper.update(boardVO));
+//			return;
+//		}
+//		log.info("NO BOARD!");
+//	}
+	
+	
 }
