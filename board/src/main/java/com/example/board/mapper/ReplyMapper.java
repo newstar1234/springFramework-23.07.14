83023d909c58d4b32bbc64d5c5414637f26b0ab2
newstar <maori36@naver.com>
2023-08-24 23:06:30 +0900

    23.08.24

--- board/src/main/java/com/example/board/mapper/ReplyMapper.java
+++ board/src/main/java/com/example/board/mapper/ReplyMapper.java
@@ -2,7 +2,12 @@ package com.example.board.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.board.domain.vo.ReplyVO;
+
 @Mapper
 public interface ReplyMapper {
 	
+	public int insert(ReplyVO replyVO);
+	public ReplyVO select(Long rno);
+
 }
--- board/src/test/java/com/example/board/mapper/ReplyMapperTests.java
+++ board/src/test/java/com/example/board/mapper/ReplyMapperTests.java
@@ -1,10 +1,14 @@
 package com.example.board.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.board.domain.vo.ReplyVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -13,11 +17,31 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
 	
+	private Long[] arBno = {1343L, 1342L, 1341L, 1340L, 1339L};
+	
 	@Autowired
 	ReplyMapper replyMapper;
 	
 	@Test
 	public void mapperTest() {
 		log.info(replyMapper);
+	}
+	
+//	@Test
+//	public void insertTest() {
+//		//5개의 게시글에 2개씩 댓글 달기
+//		IntStream.rangeClosed(1, 10).forEach(i -> {
+//			ReplyVO replyVO = new ReplyVO();
+//			replyVO.setBno(arBno[i % 5]);
+//			replyVO.setReply("댓글 테스트" + i);
+//			replyVO.setReplier("작성자" + i);
+//			
+//			replyMapper.insert(replyVO);
+//		});
+//	}
+	
+	@Test
+	public void selectTest() {
+		log.info(replyMapper.select(10L));
 	}
 }
